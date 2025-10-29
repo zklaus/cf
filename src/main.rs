c741e1c83a45133d8e930f0ccae2c549db21c6c4
Klaus Zimmermann <klaus.zimmermann@quansight.com>
2025-10-29 14:56:47 +0100

    Add basic command scaffolding

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,81 @@
+use std::process;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about = "A c-forge CLI tool", long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    Feedstock {
+        #[command(subcommand)]
+        command: FeedstockCommands,
+    },
+    StagedRecipes,
+    Status,
+}
+
+#[derive(Subcommand, Debug)]
+enum FeedstockCommands {
+    Setup,
+    Work,
+    ListPRs,
+    Rerender,
+    UpdateVersion,
+    Lint,
+    Migrator,
+    Config,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Feedstock { command } => match command {
+            FeedstockCommands::Setup => {
+                eprintln!("Command not implemented yet.")
+                process::exit(1);
+            }
+            FeedstockCommands::Work => {
+                eprintln!("Command not implemented yet.")
+                process::exit(1);
+            }
+            FeedstockCommands::ListPRs => {
+                eprintln!("Command not implemented yet.")
+                process::exit(1);
+            }
+            FeedstockCommands::Rerender => {
+                eprintln!("Command not implemented yet.")
+                process::exit(1);
+            }
+            FeedstockCommands::UpdateVersion => {
+                eprintln!("Command not implemented yet.")
+                process::exit(1);
+            }
+            FeedstockCommands::Lint => {
+                eprintln!("Command not implemented yet.")
+                process::exit(1);
+            }
+            FeedstockCommands::Migrator => {
+                eprintln!("Command not implemented yet.")
+                process::exit(1);
+            }
+            FeedstockCommands::Config => {
+                eprintln!("Command not implemented yet.")
+                process::exit(1);
+            }
+        },
+        Commands::StagedRecipes => {
+            eprintln!("Staged recipe commands are not yet implemented.");
+            process::exit(1);
+        }
+        Commands::Status => {
+            eprintln!("Status commands are not yet implemented.");
+            process::exit(1);
+        }
+    }
 }
